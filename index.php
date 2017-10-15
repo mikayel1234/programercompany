5f780a18b3caff370ef0aeeb98970e1ae5eb206e
your name <you@example.com>
2017-10-15 20:33:14 +0400

    sql test

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 	<?php
-		$result=mysqli_connect("ec2-54-243-252-91.compute-1.amazonaws.com","mbqqlybjsttlim","3b6c57d79808af21d053cc103827a32c97acb740f30bc421b079e0c43a3d71d9","d2hkvntmipukav");
+		$result=pg_connect("ec2-54-243-252-91.compute-1.amazonaws.com","mbqqlybjsttlim","3b6c57d79808af21d053cc103827a32c97acb740f30bc421b079e0c43a3d71d9");
 		if($result)
 		{
 			echo "asd";
