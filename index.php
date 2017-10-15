7656c3f12d8b5d5925a6fd88030766281b377d99
your name <you@example.com>
2017-10-15 20:24:26 +0400

    test

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 	<?php
-		$result=mysql_connect("ec2-54-243-252-91.compute-1.amazonaws.com","mbqqlybjsttlim","3b6c57d79808af21d053cc103827a32c97acb740f30bc421b079e0c43a3d71d9","d2hkvntmipukav");
+		$result=mysqli_connect("ec2-54-243-252-91.compute-1.amazonaws.com","mbqqlybjsttlim","3b6c57d79808af21d053cc103827a32c97acb740f30bc421b079e0c43a3d71d9","d2hkvntmipukav");
 		if($result)
 		{
 			echo "asd";
