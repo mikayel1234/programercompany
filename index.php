a6f25530dae9a5de52c5dada95a39ef1be197b1c
your name <you@example.com>
2017-10-18 16:06:57 +0400

    registration

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
 </head>
 <body>
 	<?php
-		$conn_string = "host=ec2-54-243-252-91.compute-1.amazonaws.com port=5432 dbname=d2hkvntmipukav user=mbqqlybjsttlim password=3b6c57d79808af21d053cc103827a32c97acb740f30bc421b079e0c43a3d71d9";
-		$dbconn4 = pg_connect($conn_string);
-		if($dbconn4)
+		require "postgresql_require.php";
+		session_start();
+		if(isset($_SESSION['email']))
 		{
-			echo "ura";
+			header("home_page.php");
+			exit;
 		}
 	?>
 </body>
