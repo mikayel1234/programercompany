7fc888a2ad25f0394ba5d1b7096c42f8fe20a04f
your name <you@example.com>
2017-10-19 18:11:48 +0400

    mail

--- adduser_form.php
+++ adduser_form.php
@@ -12,14 +12,13 @@
 	$date_registration=date("Y/m/d");
 	$link=pg_query("INSERT INTO user_info(name,lastname,email,password,year,day,month,fm,tfmail,r_hours,r_data) VALUES('$name','$lastname','$email','$password','$year','$day','$month','$gender','no',$hours,'$date_registration')");
 	//////
-	$subject = "Hello ".$name;
-                         $headers = "From: webmaster@example.com" . "\r\n" .
-                       "CC: somebodyelse@example.com";
-			
-                          $row1=mysqli_fetch_assoc($result1);
-                          $txt ='https://phpmysqljavascript.000webhostapp.com/index.php/index.php/insert_user.php?id=1';
+$to = "somebody@example.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
 
-                      mail($email,$subject,$txt,$headers);
+mail($to,$subject,$txt,$headers);
     
 	////////
 	header("Location:sendmail.php");
