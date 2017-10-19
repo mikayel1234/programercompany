0dd45a5e6e7e0f3b2a65bf1d9198643e88e323a6
your name <you@example.com>
2017-10-19 18:17:40 +0400

    mail

--- adduser_form.php
+++ adduser_form.php
@@ -12,13 +12,15 @@
 	$date_registration=date("Y/m/d");
 	$link=pg_query("INSERT INTO user_info(name,lastname,email,password,year,day,month,fm,tfmail,r_hours,r_data) VALUES('$name','$lastname','$email','$password','$year','$day','$month','$gender','no',$hours,'$date_registration')");
 	//////
-$to = "somebody@example.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: webmaster@example.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-
-mail($to,$subject,$txt,$headers);
-    
+	$subject = "Hello ".$name;
+    $headers = "From: programercompany.herokuapp.com" . "\r\n" ."CC: somebodyelse@example.com";
+    $result=pg_query("SELECT * from user_info WHERE email='$email'");
+    $row=pg_fetch_assoc($result);
+    $txt ='https://programercompany.herokuapp.com/insert_user.php?id='.$row['id'];
+    new SendGrid\Mail($email,$subject,$txt,$headers);
+    if(!$mail)
+    {
+    	header("Location:index.php");
+    }
 	////////
 	header("Location:sendmail.php");
