3d73a63962701b709936102a4dd9239ece4eb5eb
your name <you@example.com>
2017-10-19 16:39:19 +0400

    days

--- index.php
+++ index.php
@@ -30,7 +30,38 @@
 			});
 		});
 	</script>
-	<script src="birthday.js"></script>
+	<script type="text/javascript">
+		
+		$(document).ready(function(){
+var year=$("#year").val();
+			var month=$("#month").val();
+			var day=$("#day").val();
+			if(year%4!=0&&month=="February"&&day==29||year%4!=0&&month=="February"&&day==30||year%4!=0&&month=="February"&&day==31)
+			{
+				return false;
+			}
+			if(year%4==0&&month=="February"&&day==30||year%4!=0&&month=="February"&&day==31)
+			{
+				return false;
+			}
+			if(month=="April"&&day==31)
+			{
+				return false;
+			}
+			if(month=="June"&&day==31)
+			{
+				return false;
+			}
+			if(month=="September"&&day==31)
+			{
+				return false;
+			}
+			if(month=="November"&&day==31)
+			{
+				return false;
+			}
+	});
+	</script>
 </head>
 <body>
 	<?php
