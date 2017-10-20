0a093aa371eece0642e374f8409fccd66333b66b
your name <you@example.com>
2017-10-20 20:18:28 +0400

    html test

--- javascript/birthday.js
+++ javascript/birthday.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
 				var year=$("#year").val();
 			var month=$("#month").val();
 			var day=$("#day").val();
-			var name=$("#name").indexOf("<");
-			var lastname=$("#lastname").indexOf("<");
-			var login=$("#login").indexOf("<");
+			var name=$("#name").val().indexOf("<");
+			var lastname=$("#lastname").val().indexOf("<");
+			var login=$("#login").val().indexOf("<");
 			if(year%4!=0&&month=="February"&&day==29||year%4!=0&&month=="February"&&day==30||year%4!=0&&month=="February"&&day==31)
 			{
 				return false;
