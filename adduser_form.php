a255951d95eb9227f5912996a63f57bacfe9ce1b
your name <you@example.com>
2017-10-20 20:09:42 +0400

    html test

--- adduser_form.php
+++ adduser_form.php
@@ -14,7 +14,7 @@
 	$password=crypt($login,$_POST['password']);
 	$date_registration=date("Y/m/d");
 	$link=pg_query("INSERT INTO user_info(name,lastname,login,password,year,day,month,fm,r_data) VALUES('$name','$lastname','$login','$password','$year','$day','$month','$gender','$date_registration')");
-	$_SESSION['login']=$login;
+	
     header("Location:home_page.php");
 }
     catch(Exception $error)
