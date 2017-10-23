ed2f141c704f58d853fd59265b7c840709c02d34
your name <you@example.com>
2017-10-23 15:21:33 +0400

    style

--- index.php
+++ index.php
@@ -28,9 +28,7 @@
 						equalTo:"#password"
 					}
 				}
-				$(".reg_submit").click(function(){
-					$("body").css({"cursor":"progress"});
-				});
+				
 			});
 			var color=1;
      function user_count()
