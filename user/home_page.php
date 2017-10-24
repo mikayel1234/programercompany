a51c459522f1dc0c7e519c898f3a80c5e9a466c7
Your Name <you@example.com>
2017-10-24 18:01:21 +0200

    user

--- user/home_page.php
+++ user/home_page.php
@@ -10,7 +10,7 @@
 		$result_user_nl=mysql_query("SELECT * from user_info WHERE login='$login'");
 		$row_user_nl=mysql_fetch_assoc($result_user_nl);
 	?>
-	<title><?php echo $row_user_nl['name']." ".$row_user_nl['lastname']?></title>
+	<title><?php echo $row_user_nl['name']." ".$row_user_nl['lastname'];?></title>
 	<link rel="shortcut icon" href="http://www.domain.com/favicon.ico" type="image/x-icon" />
 	<link rel="stylesheet" type="text/css" href="../style_user/style.css">
 </head>
