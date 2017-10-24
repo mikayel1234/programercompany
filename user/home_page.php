cdcaa65f542e49f77379e07555aa1086b03fb323
Your Name <you@example.com>
2017-10-24 17:49:27 +0200

    user

--- user/home_page.php
+++ user/home_page.php
@@ -1,9 +1,18 @@
+<?php
+ 		session_start();
+ 		require "../postgresql_request.php";
+ 		$login=$_SESSION['login'];
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>ProgramerCompany</title>
+	<?php 
+		$result_user_nl=mysql_query("SELECT * from user_info WHERE login='$login'");
+		$row_user_nl=mysql_fetch_assoc($result_user_nl);
+	?>
+	<title><?php echo $row_user_nl['name']." ".$row_user_nl['lastname']?></title>
 	<link rel="shortcut icon" href="http://www.domain.com/favicon.ico" type="image/x-icon" />
-	<link rel="stylesheet" type="text/css" href="">
+	<link rel="stylesheet" type="text/css" href="../style_user/style.css">
 </head>
 <body>
 
