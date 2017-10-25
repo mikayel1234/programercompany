d03441c24954959528ce70d8d053c8de2259a513
your name <you@example.com>
2017-10-25 19:50:24 +0400

    user

--- user/home_page.php
+++ user/home_page.php
@@ -6,12 +6,15 @@
 		session_start();
 		require "../postgresql_require.php";
 		$login=$_SESSION['login'];
+		echo $login;
 		$result_user_nm=pg_query("SELECT * from user_info WHERE login='$login'");
 		$row_nl=pg_fetch_assoc($result_user_nm);
 		$name=$row_nl['name'];
+		echo $name;
 		$lastname=$row_nl['lastname'];
+		echo $lastname;
 	?>
-	<title><?php echo $name." ".$lastname?></title>
+	<title><?php echo $name." ".$lastname;?></title>
 	<link rel="shortcut icon" href="http://www.domain.com/favicon.ico" type="image/x-icon" />
 	<link rel="stylesheet" type="text/css" href="../style_user/style.css">
 </head>
