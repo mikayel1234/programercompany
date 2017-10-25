1d2e3fccb829f46cae0b1dd31666d0f3c926527b
your name <you@example.com>
2017-10-25 19:48:45 +0400

    user

--- user/home_page.php
+++ user/home_page.php
@@ -2,8 +2,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	
-	<title>sdf</title>
+	<?php
+		session_start();
+		require "../postgresql_require.php";
+		$login=$_SESSION['login'];
+		$result_user_nm=pg_query("SELECT * from user_info WHERE login='$login'");
+		$row_nl=pg_fetch_assoc($result_user_nm);
+		$name=$row_nl['name'];
+		$lastname=$row_nl['lastname'];
+	?>
+	<title><?php echo $name." ".$lastname?></title>
 	<link rel="shortcut icon" href="http://www.domain.com/favicon.ico" type="image/x-icon" />
 	<link rel="stylesheet" type="text/css" href="../style_user/style.css">
 </head>
