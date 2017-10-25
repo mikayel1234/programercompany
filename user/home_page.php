ebba0c8a7fa61074ddede5adbaf7c01962f405b5
your name <you@example.com>
2017-10-25 19:53:15 +0400

    user

--- user/home_page.php
+++ user/home_page.php
@@ -4,10 +4,9 @@
 <head>
 	<?php
 		session_start();
-		echo 1;
 		require "../postgresql_require.php";
-		echo "string";
 		$login=$_SESSION['login'];
+		echo 1;
 		echo $login;
 		$result_user_nm=pg_query("SELECT * from user_info WHERE login='$login'");
 		$row_nl=pg_fetch_assoc($result_user_nm);
