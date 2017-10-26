6d31173dec81a3ae280eb3b177c0e3afc678cbf5
your name <you@example.com>
2017-10-26 17:17:22 +0400

    shut

--- user/shutdown_user.php
+++ user/shutdown_user.php
@@ -7,7 +7,7 @@
 	$year=date("Y");
 	$mont=date("m");
 	$day=date("d");
-	$query=pg_query("UPDATE online SET hour=$hours,minute=$minute,day=$day,month=$month,year=$year WHERE login='$login'");
+	$query=pg_query("UPDATE online SET hour=$hours,minute=$minute,day=$day,month=$month,year=$year WHERE user_login='$login'");
 	$_SESSION['login']="";
 	header("Location:../index.php");
 ?>
