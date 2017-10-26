caa47727ed579fadd9040e264fce47b929288d08
your name <you@example.com>
2017-10-26 17:15:52 +0400

    shut

--- user/shutdown_user.php
+++ user/shutdown_user.php
@@ -1,6 +1,13 @@
 <?php
 	require "../postgresql_require.php";
 	session_start();
+	$login=$_SESSION['login'];
+	$hours=date("h");
+	$minute=date("i");
+	$year=date("Y");
+	$mont=date("m");
+	$day=date("d");
+	$query=pg_query("UPDATE online SET hour=$hours,minute=$minute,day=$day,month=$month,year=$year WHERE login='$login'");
 	$_SESSION['login']="";
 	header("Location:../index.php");
 ?>
