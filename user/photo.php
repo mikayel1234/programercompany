16293fae00a785e79605f8041f7b2affb50a2b9a
your name <you@example.com>
2017-10-27 19:54:48 +0400

    pfoho

--- user/photo.php
+++ user/photo.php
@@ -13,8 +13,8 @@
 			{
 				$time++;
 			}
-			echo $_FILES["img_1_u"]["tmp_name"];
-			if(@move_uploaded_file($_FILES["img_1_u"]["tmp_name"],$upload_1_img))
+			echo $_FILES["img_1_u"]["t"];
+			if(move_uploaded_file($_FILES["img_1_u"]["tmp_name"],$upload_1_img))
 				echo "string";
 		}
 	}
