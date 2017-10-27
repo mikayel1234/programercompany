c1c8d1f604e284c439b770183ddee9399bc359cc
your name <you@example.com>
2017-10-27 18:10:52 +0400

    img

--- user/photo.php
+++ user/photo.php
@@ -1,8 +1,14 @@
 <?php
-	include "include_header.php"
+	include "include_header.php";
+	require '../postgresql_require.php';
+	session_start();
+	$login=$_SESSION['login'];
+	$img_1_user=pg_query("SELECT * from user_img WHERE login='$login'");
+	$img_1_href=pg_fetch_assoc($img_1_user);
 ?>
 <div>
 	<h1>photo</h1>
+	<img src="<?php echo $img_1_href['href']?>">
 </div>
 </body>
 </html>
