65232d0fdb48c876b17c7fd63ad2f391a963043b
your name <you@example.com>
2017-10-27 18:38:56 +0400

    img

--- user/photo.php
+++ user/photo.php
@@ -3,12 +3,31 @@
 	require '../postgresql_require.php';
 	session_start();
 	$login=$_SESSION['login'];
+	if(isset($_POST['add_1_img']))
+	{
+		if(@getimagesize($_FILES['img_1_u']['tmp_name']))
+		{
+			$user_1_img=$_FILES['img_1_u']['name'];
+			$time=time();
+			while(file_exists($upload_1_img="photo_1/".$time."-".$user_1_img))
+			{
+				$time++;
+			}
+			@move_uploaded_file($_FILES["img_1_u"]["tmp_name"],$upload_1_img);
+		}
+	}
 	$img_1_user=pg_query("SELECT * from user_img WHERE login='$login'");
 	$img_1_href=pg_fetch_assoc($img_1_user);
 ?>
 <div>
 	<h1>photo</h1>
 	<img src="<?php echo $img_1_href['href']?>">
+	<form action="photo.php" method="post" enctype="multipart/form-data">
+		<input type="text" name="comment">
+		<input type="file" name="img_1_u">
+		<input type="submit" name="add_1_img">
+
+	</form>
 </div>
 </body>
 </html>
