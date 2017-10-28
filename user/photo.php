04bbff985e53f60ae08b432da12836b6ff5ccb24
your name <you@example.com>
2017-10-28 16:50:31 +0400

    photo

--- user/photo.php
+++ user/photo.php
@@ -7,14 +7,14 @@
 	{
 		if(@getimagesize($_FILES['img_1_u']['tmp_name']))
 		{
-			$image = $_FILES["img_1_u"];
-			$image_filename = $image['name'];
-			$image_info = getimagesize($image['tmp_name']);
-			$image_mime_type = $image_info['mime'];
-			$image_size = $image['size'];
-			$image_data = file_get_contents($image['tmp_name']);
-			
-		$insert_1_img=pg_query_params($dbconn4,"INSERT INTO user_1_photo(login,image_name,image_size,image_mime,image_data) VALUES('$login','$image_filename',$image_size,'$image_mime_type','$image_data')");
+			$filename=$_FILES['img_1_u']['name'];
+				$time=time();
+				while (file_exists($upload=$time.'-'.$filename) )
+				{
+					$time++;
+				}
+				$color=$_POST['name'.$i];
+				@move_uploaded_file($_FILES['img_1_u']['tmp_name'],$upload);
 		}
 			
 	}
