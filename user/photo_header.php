e32141522dc80670f464cc6524f9cad8743287f4
your name <you@example.com>
2017-10-28 14:57:28 +0400

    phto

--- user/photo_header.php
+++ user/photo_header.php
@@ -4,7 +4,7 @@
 	$login=$_SESSION['login'];
 	$result=pg_query("SELECT * from user_1_photo WHERE login='$login'");
 	$image=pg_fetch_assoc($result);
-	header('Content-length:' . $image['image_size']);
+	
 	header('Content-type:' . $image['image_mime']);
 echo $image['image_data'];
 
