3a6a8084da912b59e234fc26b0bc6c24da3777be
your name <you@example.com>
2017-10-28 13:36:13 +0400

    phto

--- user/photo_header.php
+++ user/photo_header.php
@@ -1,7 +1,10 @@
 <?php
 	require '../postgresql_require.php';
-	
-	
-
-
+	session_start();
+	$login=$_SESSION['login'];
+	$result=pg_query("SELECT * from user_1_photo WHERE login='$login'");
+	$image=pg_fetch_assoc($result);
+	header('Content-type: ' . $image['image_mime']);
+header('Content-length: ' . $image['image_size']);
+echo $image['imgae_data'];
 ?>
