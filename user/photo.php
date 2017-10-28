5d30cc1b240db8edf932274e80e699fad7dda123
your name <you@example.com>
2017-10-28 12:38:39 +0400

    phto

--- user/photo.php
+++ user/photo.php
@@ -13,7 +13,7 @@
 			$image_mime_type = $image_info['mime'];
 			$image_size = $image['size'];
 			echo $image_filename;
-			$image_data = pg_real_escape_string(file_get_contents($image['tmp_name']));
+			$image_data = file_get_contents($image['tmp_name']);
 			echo $image_data;
 			$insert_1_img=pg_query("INSERT INTO user_1_photo(image_name,image_size,image_mime) VALUES('$image_filename',$image_size,'$image_mime_type')");
 		}
