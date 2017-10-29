f47d5ba81123726c6da69801c8edd8f4ed06e13d
your name <you@example.com>
2017-10-29 11:14:38 +0300

    search

--- user/friends_search.php
+++ user/friends_search.php
@@ -1,7 +1,10 @@
 <?php
 	include "include_header.php";
 	$search=$_POST['friend'];
-	$result1=pg_query("SELECT * from user_info WHERE (name+' '+lastname) ='$search'");
+	$nf=explode(' ',$search);
+	$name=$nf[0];
+	$lastname=$nf[1];
+	$result1=pg_query("SELECT * from user_info WHERE name='$name' AND lastname='$lastname'");
 	echo $search;
 	echo pg_num_rows($result1);
 	
