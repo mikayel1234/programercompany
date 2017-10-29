3a978b7b2c23c350dea1a5e8f76b31b76debda99
your name <you@example.com>
2017-10-29 10:26:31 +0300

    search

--- user/friends_search.php
+++ user/friends_search.php
@@ -1,7 +1,7 @@
 <?php
 	include "include_header.php";
 	$search=$_POST['friend'];
-	$result=pg_query("SELECT * from user_info WHERE (name + ' ' + lastname) like '%$search%'");
+	$result=pg_query("SELECT * from user_info WHERE (name + ' ' + lastname) =$search");
 	while($row=pg_fetch_assoc($result);)
 	{
 		echo $row['name']." ".$row['lastname'];
