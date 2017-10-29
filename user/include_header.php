e809a96183061569a44be6beaba80c9d296b9176
your name <you@example.com>
2017-10-29 15:53:48 +0300

    shut

--- user/include_header.php
+++ user/include_header.php
@@ -38,7 +38,7 @@
 <div id="header_reg">
 	<h1 id="header_index">Programer Company</h1>
 </div>
-<a href="shutdown_user.php"><img src="../images/logout-256.png" ></a>
+<a href="shutdown_user.php"><img src="../images/images.jpg" ></a>
 <img src="<?php echo $img_href?>">
 <ul>
 	<li><a href="home_page.php">Public</a></li>
