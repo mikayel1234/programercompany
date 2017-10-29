c90e4d2cf6e0c319a737c85377ab4b79aed85bb5
your name <you@example.com>
2017-10-29 10:01:07 +0300

    search

--- user/search_user.php
+++ user/search_user.php
@@ -2,10 +2,10 @@
 	require "../postgresql_require.php";
 	if(isset($_GET['term']))
 	{
-		$array=array();
+		$array=[];
 		$term=$_GET['term'];
 		$result=pg_query("SELECT * from user_info WHERE name LIKE '$term%'");
-		$result1=pg_query("SELECT * from user_info WHERE lastname LIKE '$term%'");
+		
 		$i=0;
 		while($row=mysql_fetch_assoc($result))
 		{	
