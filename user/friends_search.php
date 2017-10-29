b5808436d45feb2d7ee02287b1b877e96c054145
your name <you@example.com>
2017-10-29 10:46:35 +0300

    search

--- user/friends_search.php
+++ user/friends_search.php
@@ -1,7 +1,7 @@
 <?php
 	include "include_header.php";
 	$search=$_POST['friend'];
-	$result=pg_query("SELECT * from user_info WHERE (name+' '+lastname) ='$search'");
+	$result=pg_query("SELECT * from user_info WHERE MATCH(name, lastname) AGAINST ('$search')");
 	echo $search;
 	while($row=pg_fetch_assoc($result))
 	{
