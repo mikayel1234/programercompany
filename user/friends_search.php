9293e614e69dcb604163258049555b1683e823c1
your name <you@example.com>
2017-10-29 11:01:19 +0300

    search

--- user/friends_search.php
+++ user/friends_search.php
@@ -1,9 +1,9 @@
 <?php
 	include "include_header.php";
 	$search=$_POST['friend'];
-	$result1=pg_query("SELECT CONCAT(name,' ', lastname) AS 'fullname'from user_info WHERE fullname ='$search'");
+	$result1=pg_query("SELECT CONCAT(name,' ', lastname) AS 'fullname' from user_info WHERE fullname =$search");
 	echo $search;
-	while($row=pg_fetch_assoc($result1))
+	while($row1=pg_fetch_assoc($result1))
 	{
 		echo 1;
 	}
