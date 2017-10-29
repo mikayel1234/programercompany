8da7771858a21dc00d4d7940a5cc7f1e0aa68b43
your name <you@example.com>
2017-10-29 11:05:30 +0300

    search

--- user/friends_search.php
+++ user/friends_search.php
@@ -1,12 +1,10 @@
 <?php
 	include "include_header.php";
 	$search=$_POST['friend'];
-	$result1=pg_query("SELECT * from user_info WHERE CONCAT(name+' '+lastname) ='$search'");
+	$result1=pg_query("SELECT * from user_info WHERE name+' '+lastname ='$search'");
 	echo $search;
-	while($row=pg_fetch_assoc($result1))
-	{
-		echo 1;
-	}
+	echo pg_num_rows($result1);
+	
 ?>
 </body>
 </html>
