16beba3fc44e04d963e65323a1bfd5dfd09fe3e3
your name <you@example.com>
2017-10-29 10:05:15 +0300

    search

--- user/search_user.php
+++ user/search_user.php
@@ -2,10 +2,21 @@
 	require "../postgresql_require.php";
 	if(isset($_GET['term']))
 	{
-		$array=[];
+		$array=array();
 		$term=$_GET['term'];
 		$result=pg_query("SELECT * from user_info WHERE name LIKE '$term%'");
-		
-		
+		$result1=pg_query("SELECT * from user_info WHERE lastname LIKE '$term%'");
+		$i=0;
+		while($row=pg_fetch_assoc($result))
+		{	
+			$array[$i]=$row['name'];
+			$i++;
+		}
+		while($row1=pg_fetch_assoc($result1))
+		{	
+			$array[$i]=$row1['name'];
+			$i++;
+		}
+		echo json_encode($array);
 	}
 ?>
