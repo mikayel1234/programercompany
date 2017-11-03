d2f43e82b556bcc4370d2b10fa4651e863ca8d4b
your name <you@example.com>
2017-11-03 18:40:51 +0300

    req

--- user/require_friend.php
+++ user/require_friend.php
@@ -8,7 +8,6 @@
 	}
 	$login=$_SESSION['login'];
 	$id=$_GET['id'];
-	$add_table_req=pg_query("INSERT INTO friend_answer(fr_id,login) VALUES($id,'$login')");
-	header("Location:../index.php");
-	exit;
+	
+
 ?>
