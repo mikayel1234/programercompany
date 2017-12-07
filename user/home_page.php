cb49c757c3d74d3792af2cb5ff3e2d546cab6dd2
your name <you@example.com>
2017-12-07 16:07:28 +0300

    sd

--- user/home_page.php
+++ user/home_page.php
@@ -4,15 +4,16 @@
 <div>
 	
 			<input type="text" name="desc" id="e" >
+			<input type="text" name="usr" id="bc">
 		<button id="abc">Send</button>
 	<script type="text/javascript">
 		$("#abc").click(function(){
 			var desk=$("#e").val();
-			
+			var desk1=$("#bc").val();
 			var http=new XMLHttpRequest();
 			http.open("POST","chat.php",true);
 			http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
- 			http.send("desc="+desk);
+ 			http.send("desc="+desk+"&desk1");
  			$("#e").val("");
 		});
 	</script>
--- user/zap.php
+++ user/zap.php
@@ -3,14 +3,20 @@
 	$ps=pg_query("SELECT * from chat");
 	$a="";
 	$array=[];
+	session_start();
+	$mail=$_SESSION['login'];
 	$i=0;
 	while($row=pg_fetch_assoc($ps))
 	{
-		$d=$row['description'];
-		$a=$row['username']."----".$d;	
-		$array[$i]=$a;
-		$i++;
-		$a="";
+		if($row['usr']==$mail)
+		{
+			$d=$row['description'];
+			$a=$row['username']."----".$d;	
+			$array[$i]=$a;
+			$i++;
+			$a="";
+		}
+		
 	}
 	echo json_encode($array);
 ?>
