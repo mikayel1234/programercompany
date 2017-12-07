fe96e9030a3f5cdf1476baf48446e8e4d551fda1
your name <you@example.com>
2017-12-07 16:15:19 +0300

    sd

--- user/chat.php
+++ user/chat.php
@@ -3,6 +3,7 @@
 	session_start();
 	$login=$_SESSION['login'];
 	$desc=$_POST["desc"];
+	$desc1=$_POST["desc1"];
 	$ps=pg_query("INSERT INTO chat(username,description) VALUES('$login','$desc','$desc1')");
 	
 ?>
