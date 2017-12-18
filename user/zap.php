9effedc3777c3fbd2620dda56c0f516ecde70642
mikayel1234 <32797689+mikayel1234@users.noreply.github.com>
2017-12-18 18:38:47 +0400

    Update zap.php

--- user/zap.php
+++ user/zap.php
@@ -1,10 +1,11 @@
 <?php
 	require "../postgresql_require.php";
-	$ps=pg_query("SELECT * from chat WHERE usr='$mail'or username='$login'");
-	$a="";
-	$array=[];
 	session_start();
 	$mail=$_SESSION['login'];
+	$ps=pg_query("SELECT * from chat WHERE usr='$mail'or username='$mail'");
+	$a="";
+	$array=[];
+	
 	$i=0;
 	while($row=pg_fetch_assoc($ps))
 	{
